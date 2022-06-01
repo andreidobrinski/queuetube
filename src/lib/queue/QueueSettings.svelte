4cffbfb6cc28233515d1fc59650064e22d84ec20
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-05-31 20:04:58 -0400

    add delete functions

--- src/lib/queue/QueueSettings.svelte
+++ src/lib/queue/QueueSettings.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { fade, crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 	import IconButton from '@smui/icon-button';
 	import Button from '@smui/button';
 	import type { SubscriptionResponse, SubscriptionItem } from '$lib/types';
@@ -6,6 +8,8 @@
 	import { queueStore } from '$lib/queueStore';
 	import { selectedQueue } from '$lib/selectedQueue';
 	import ChannelImage from '$lib/queue/ChannelImage.svelte';
+
+	const [send, receive] = crossfade({});
 
 	let subscriptions: Array<SubscriptionItem> = [];
 
@@ -85,6 +89,29 @@
 		subscriptions = subscriptions.filter((subscription) => subscription.id !== channel.id);
 	}
 
+	function deleteChannel(channelId: string) {
+		queueStore.update((queues) => {
+			const currentQueue = queues[$selectedQueue];
+			const { [channelId]: deletedChannel, ...remainderChannels } = currentQueue.channels;
+			return {
+				...queues,
+				[$selectedQueue]: {
+					...currentQueue,
+					channels: remainderChannels,
+				},
+			};
+		});
+	}
+
+	function deleteQueue() {
+		const queueId = $selectedQueue;
+		selectedQueue.set('');
+		queueStore.update((queues) => {
+			const { [queueId]: queue, ...rest } = queues;
+			return rest;
+		});
+	}
+
 	$: channelNumber = Object.keys($queueStore[$selectedQueue].channels).length;
 </script>
 
@@ -96,7 +123,12 @@
 	<p>{channelNumber} Channels in Queue</p>
 {/if}
 {#each Object.values($queueStore[$selectedQueue].channels) as channel (channel.id)}
-	<div style="display: flex; align-items: center;">
+	<div
+		style="display: flex; align-items: center;"
+		animate:flip
+		in:receive={{ key: channel.id }}
+		out:fade
+	>
 		<ChannelImage
 			src={channel.thumbnails?.default.url || channel.thumbnails?.medium.url || ''}
 			name={channel.name}
@@ -105,6 +137,7 @@
 		<IconButton
 			aria-label={`delete channel ${channel.name} from queue ${$selectedQueue}`}
 			class="material-icons"
+			on:click={() => deleteChannel(channel.id)}
 		>
 			delete
 		</IconButton>
@@ -117,7 +150,12 @@
 	<p>My Subscriptions</p>
 {/if}
 {#each subscriptions as subscription (subscription.id)}
-	<div style="display: flex; align-items: center;">
+	<div
+		style="display: flex; align-items: center;"
+		animate:flip
+		in:receive={{ key: subscription.snippet.resourceId.channelId }}
+		out:send={{ key: subscription.snippet.resourceId.channelId }}
+	>
 		<IconButton
 			on:click={() => addChannel(subscription)}
 			aria-label={`add channel ${subscription.snippet.title} to queue ${$selectedQueue}`}
@@ -133,3 +171,5 @@
 		<p>{subscription.snippet.title}{subscription.contentDetails.newItemCount ? ' - *' : ''}</p>
 	</div>
 {/each}
+
+<Button on:click={deleteQueue}>Delete Queue</Button>
