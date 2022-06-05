2bc5cc1855089f74e581af9853d51627b8f728dc
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-06-05 18:50:00 -0400

    Updates

---
+++ _app/chunks/authStore-30b5ead7.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-ec931ad3.js";const o={token:localStorage.token||"",isLoggedIn:!1},a=e(o);a.subscribe(t=>localStorage.setItem("token",t.token));export{a};
--- _app/chunks/authStore-87f19da0.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index-39dab4a5.js";const o={token:localStorage.token||"",isLoggedIn:!1},a=e(o);a.subscribe(t=>localStorage.setItem("token",t.token));export{a};
