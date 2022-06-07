a2edc49520379eedffe20b5e96c9c816bcb41f9e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-06-07 19:38:42 -0400

    sort queuelist alphabetically

--- src/lib/queueList/QueueList.svelte
+++ src/lib/queueList/QueueList.svelte
@@ -3,11 +3,13 @@
 	import { queueStore } from '$lib/queueStore';
 	import AddQueue from '$lib/queueList/AddQueue.svelte';
 	import { selectedQueue } from '$lib/selectedQueue';
+
+	$: sortedQueues = Object.values($queueStore).sort((a, b) => a.name.localeCompare(b.name));
 </script>
 
 <h2 class="mdc-typography--headline5">My Queue List</h2>
 <div style="display: flex; flex-direction: column; align-items: flex-start; margin-bottom: 12px;">
-	{#each Object.values($queueStore) as queue}
+	{#each sortedQueues as queue}
 		<Button color="secondary" on:click={() => selectedQueue.set(`${queue.name}`)}>
 			{queue.name}
 		</Button>
