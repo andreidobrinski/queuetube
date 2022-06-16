26457bc9e24b1a8f0b525cf52bd177acbe4f3ba8
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-06-15 23:28:23 -0400

    add logout

---
+++ src/routes/logout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { authStore } from '$lib/auth/authStore';
+	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
+
+	authStore.set({
+		token: '',
+		isLoggedIn: false,
+	});
+
+	goto(`${base}/`);
+</script>
