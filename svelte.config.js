3dafe245eb367cf9e0c1bcc4d82f46f3926a3ae2
andreidobrinski <andreiidobrinski@gmail.com>
2023-12-26 18:33:53 -0500

    add 404 fallback

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html',
-			// fallback: null,
-			strict: false,
+			fallback: '404.html',
 		}),
 		paths: {
 			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
