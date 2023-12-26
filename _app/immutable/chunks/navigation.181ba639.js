7189ca97b5b371ec8c4c892c34a760da8f64603f
andreidobrinski <andreidobrinski@users.noreply.github.com>
2023-12-26 22:19:33 +0000

    deploy: 0de7f2526017f8352a9e79e8a1f931fd67457995

--- _app/immutable/chunks/navigation.181ba639.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.a15ab410.js";const e=o("goto");export{e as g};
--- _app/immutable/chunks/paths.2417eed1.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1ru6gjj)==null?void 0:s.base)??"/queuetube";var e;const a=((e=globalThis.__sveltekit_1ru6gjj)==null?void 0:e.assets)??t;export{a,t as b};
